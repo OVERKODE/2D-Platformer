cabdab3db79137a39c2412721138536b60740ddd
OVERKODE <honaleksandr4@gmail.com>
2024-08-18 00:43:15 +0300

    s

---
+++ SCRIPTS/menu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+func _on_start_pressed() -> void:
+	#get_tree().change_scene_to_file("")
+	pass
+
+func _on_levels_pressed() -> void:
+	#get_tree().change_scene_to_file("")
+	pass

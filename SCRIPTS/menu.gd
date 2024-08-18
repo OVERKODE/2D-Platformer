9acb87ac2cb929f32626890895a0876b3b8ea60f
OVERKODE <honaleksandr4@gmail.com>
2024-08-18 15:14:07 +0300

    player + menu

--- SCRIPTS/menu.gd
+++ SCRIPTS/menu.gd
@@ -2,9 +2,4 @@ extends Node2D
 
 
 func _on_start_pressed() -> void:
-	#get_tree().change_scene_to_file("")
-	pass
-
-func _on_levels_pressed() -> void:
-	#get_tree().change_scene_to_file("")
-	pass
+	get_tree().change_scene_to_file("res://SCENES/lvl_1.tscn")
---
+++ SCRIPTS/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+@export var anim: AnimatedSprite2D
+@export var audio_listener: AudioListener2D
+@export var player_audio_stream: AudioStreamPlayer2D
+
+func movement(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta		
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction == -1:
+		$AnimatedSprite2D.flip_h = true
+	elif direction == 1:
+		$AnimatedSprite2D.flip_h = false
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+	
+func _physics_process(delta: float) -> void:
+	movement(delta)

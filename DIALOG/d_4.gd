64fdf5d73d4b91ad01a0d24fcd1aef6fb1db4cb6
OVERKODE <honaleksandr4@gmail.com>
2024-08-21 18:23:26 +0300

    катсцена

---
+++ DIALOG/d_4.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func _on_next_pressed() -> void:
+	get_tree().change_scene_to_file("res://DIALOG/D_SCENES/d_5.tscn")
--- SCRIPTS/menu.gd
+++ SCRIPTS/menu.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://SCENES/lvl_1.tscn")
+	get_tree().change_scene_to_file("res://DIALOG/D_SCENES/d_1.tscn")
